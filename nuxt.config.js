3ccc0ef8adc89f0e7d93753bee4242becbf617d4
Vincent De Oliveira <vincent@iamvdo.me>
2017-10-12 11:39:33 +0200

    Meta commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'houdini',
+    title: 'CSS Houdini Experiments · @iamvdo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Experimenting with CSS Houdini Paint API' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
   /*
